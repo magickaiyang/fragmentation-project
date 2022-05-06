b5473adaa59d0830f80f20c20634bfb6b6d0d580
Pei Li <peili@andrew.cmu.edu>
2022-05-06 16:01:28 -0400

    Add graphbig

---
+++ graphBIG/common/SIM.h
@@ -0,0 +1,15 @@
+#ifndef _SIM_H
+#define _SIM_H
+#include <stdint.h>
+#include <iostream>
+
+#ifndef SIM
+#define SIM
+#endif
+
+extern "C" unsigned __attribute__ ((noinline)) SIM_BEGIN(bool i);
+extern "C" unsigned __attribute__ ((noinline)) SIM_END(bool i); 
+
+extern "C" void __attribute__ ((noinline)) SIM_LOCK(bool * i);
+extern "C" void __attribute__ ((noinline)) SIM_UNLOCK(bool * i);
+#endif
